3da9ab6b1e7fcb4fad2381a66311e2258f691ff8
Katy Gibson <klgibson528@gmail.com>
2018-06-11 13:20:04 -0500

    Adding to github

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+const Contact = ({ text }) => <li>{text}</li>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      newContact: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const contacts = [...this.state.contacts, this.state.newContact];
+    this.setState({ contacts, newContact: "" });
+  }
+  render() {
+    const { newContact } = this.state;
+    const contacts = this.state.contacts.map((t, i) => (
+      <Contact key={i} text={t} />
+    ));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Contact List</h1>
+        </header>
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            Name:{" "}
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            E-mail:{" "}
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            Phone Number:{" "}
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            Address:{" "}
+            <input
+              type="text"
+              name="address"
+              value={this.state.address}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            City:{" "}
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            State:{" "}
+            <input
+              type="text"
+              name="state"
+              value={this.state.state}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            Zip Code:{" "}
+            <input
+              type="text"
+              name="zip"
+              value={this.state.zip}
+              onChange={e => {
+                this.setState({ [e.target.name]: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
+        <div>
+          <ol>{contacts}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
