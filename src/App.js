493e1176d9eb42ef0e6d41447851d14593119b92
Katy Gibson <klgibson528@gmail.com>
2018-06-12 19:34:39 -0500

    finished app, need to refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 import "./App.css";
 
-const Contact = ({ text }) => <li>{text}</li>;
+function Contact(props) {
+  return (
+    <li className="single-contact">
+      <div>
+        <h4>{props.contactInfo.name}</h4>
+        <h6>Email: {props.contactInfo.email}</h6>
+        <h6>Phone Number: {props.contactInfo.phone}</h6>
+        <h6>Address: {props.contactInfo.address}</h6>
+        <h6>City: {props.contactInfo.city}</h6>
+        <h6>State: {props.contactInfo.state}</h6>
+        <h6>Zip Code: {props.contactInfo.zip}</h6>
+      </div>
+    </li>
+  );
+}
 
 class App extends Component {
   constructor(props) {
@@ -21,18 +35,20 @@ class App extends Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    const info = [
-      this.state.name,
-      this.state.email,
-      this.state.phone,
-      this.state.address,
-      this.state.city,
-      this.state.state,
-      this.state.zip
-    ];
+    const info = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      address: this.state.address,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip
+    };
     const contacts = [...this.state.contacts, info];
+
+    console.log(contacts);
     this.setState({
-      contacts,
+      contacts: contacts,
       name: "",
       email: "",
       phone: "",
@@ -43,99 +59,106 @@ class App extends Component {
     });
   }
   render() {
-    const { newContact } = this.state;
-    const contacts = this.state.contacts.map((t, i) => (
-      <Contact key={i} text={t} />
-    ));
+    var contacts = [];
+    if (this.state.contacts.length) {
+      contacts = this.state.contacts.map((contact, index) => {
+        return <Contact key={"contact_" + index} contactInfo={contact} />;
+      });
+    }
+    console.log(contacts);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Contact List</h1>
+        <header className="App-header navbar-dark bg-primary">
+          <h1>Contact List</h1>
         </header>
-
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            E-mail:{" "}
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            Phone Number:{" "}
-            <input
-              type="text"
-              name="phone"
-              value={this.state.phone}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            Address:{" "}
-            <input
-              type="text"
-              name="address"
-              value={this.state.address}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            City:{" "}
-            <input
-              type="text"
-              name="city"
-              value={this.state.city}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            State:{" "}
-            <input
-              type="text"
-              name="state"
-              value={this.state.state}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            Zip Code:{" "}
-            <input
-              type="text"
-              name="zip"
-              value={this.state.zip}
-              onChange={e => {
-                this.setState({ [e.target.name]: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <input type="submit" />
-          </div>
-        </form>
-        <div>
+        <div className="contact">
+          <form onSubmit={this.handleSubmit}>
+            <div className="inputs">
+              Name:
+              <input
+                className="input"
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              E-mail:
+              <input
+                className="input"
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              Phone Number:
+              <input
+                type="text"
+                name="phone"
+                value={this.state.phone}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              Address:
+              <input
+                type="text"
+                name="address"
+                value={this.state.address}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              City:
+              <input
+                type="text"
+                name="city"
+                value={this.state.city}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              State:
+              <input
+                type="text"
+                name="state"
+                value={this.state.state}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div className="inputs">
+              Zip Code:
+              <input
+                type="text"
+                name="zip"
+                value={this.state.zip}
+                onChange={e => {
+                  this.setState({ [e.target.name]: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <input className="btn btn-primary" type="submit" />
+            </div>
+          </form>
+        </div>
+        <div className="contact-list">
           <ol>{contacts}</ol>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import AppBar from 'material-ui/AppBar';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import "bootstrap/dist/css/bootstrap.min.css";
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
